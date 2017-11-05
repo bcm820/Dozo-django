8b8b1e0a845f81c1a34e8be0cc521e31b6bcd0af
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-11-04 23:37:41 -0400

    Another tweak

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -227,9 +227,7 @@ def go(request):
         # (to account for 1 minute rule)
         session.end = timezone.now()
         session.act_duration = session.end - session.start
-        amt = 0
-        for assignment in session.assignments:
-            amt+= 1
+        amt = done.count()
         if session.act_duration < timedelta(minutes=amt)
             session.act_duration = timedelta(minutes=amt)
         if session.act_duration < session.time_challenge:
